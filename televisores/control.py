adb313b2c45d5a1f8fa3882e3f5edcd6657156e3
santiagogiraldo128 <santiagogiraldo128@gmail.com>
2023-08-25 18:55:46 -0500

    primera prueba

---
+++ televisores/control.py
@@ -0,0 +1,37 @@
+class Control:
+    def __init__(self):
+        self._tv = None
+
+    def turnOn(self):
+        self._tv.turnOn()
+
+    def turnOff(self):
+        self._tv.turnOff()
+        
+    def canalUp(self):
+        self._tv.canalUp()
+
+    def canalDown(self):
+        self._tv.canalDown()
+
+    def volumenUp (self):
+        self._tv.volumenUp()
+
+    def volumenDown(self):
+        self._tv.volumenDown()
+
+    def setCanal(self, canal):
+        self._tv.setCanal(canal)
+
+    def setVolumen(self, volumen):
+        self._tv.setVolumen(volumen)
+
+    def enlazar(self, tv):
+        self._tv = tv
+        tv.setControl(self)
+
+    def getTv(self):
+        return self._tv
+    
+    def setTv(self, tv):
+        self._tv = tv
